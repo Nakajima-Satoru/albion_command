e0b7cf7e1bf3d5921f5af70437aaadc174c2dd6f
nakatsuji masato <nakatsuji@teastalk.jp>
2021-04-15 18:29:57 +0900

    4/15

--- template/default/app/Controller/ValidateController.js
+++ template/default/app/Controller/ValidateController.js
@@ -1,5 +1,4 @@
 const { Controller } = require("albion");
-const Validator = require("albion_validator");
 
 module.exports = class ValidateController extends Controller{
 
@@ -11,21 +10,13 @@ module.exports = class ValidateController extends Controller{
 
     index(){
 
+        this.ro.setData("validate",{});
+
         if(this.ro.post.get()){
             var post=this.ro.post.get();
 
-            var testValidator = this.Validator.get("Test");
-
-console.log(testValidator);
-
-            var v=new Validator();
-
-            v
-                .addRule("value1","required","value1は必須です")
-                .addRule("value2",["minLength",6],"6文字以上を入力してください")
-                .addRule("value3",["maxLength",100],"100文字以内で入力してください");
-
-            var vres=v.verify(post);
+            var vres = this.Validator.get("Test")
+                .verify(post);
 
             if(vres.get()){
                 this.ro.setData("validate",vres);
---
+++ template/default/app/Validator/TestValidator.js
@@ -0,0 +1,30 @@
+const { Validator } = require("albion");
+
+module.exports = class TestValidator extends Validator{
+
+    beforeValidate(data){
+
+        this.rules={
+            value1:{
+                0:{
+                    rule:"required",
+                    message:"value1 is not entered.",
+                },
+            },
+            value2:{
+                0:{
+                    rule:["minLength",6],
+                    message:"Enter at least 6 characters.",
+                },
+            },
+            value3:{
+                0:{
+                    rule:["maxLength",20],
+                    message:"Enter within 20 characters.",
+                },
+            },
+        };
+
+    }
+
+};
