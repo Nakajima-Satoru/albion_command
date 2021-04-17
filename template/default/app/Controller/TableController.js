947276ab77428d5504a8d04ffd7fb8f9dda7bf4a
nakatsuji masato <nakatsuji@teastalk.jp>
2021-04-17 09:42:56 +0900

    4/17

--- template/default/app/Controller/TableController.js
+++ template/default/app/Controller/TableController.js
@@ -11,25 +11,37 @@ module.exports = class TableController extends Controller{
 
     index(){
 
-        var TestTable = this.Table.get("Test");
+        this.Table.load("Test");
         
-/*
+        //this._pattern1();
+        //this._pattern2();
+
+        this._show();
+
+        this.ro.exit();
+    }
+
+    _pattern1(){
+
         this.wait();
 
         var cont=this;
-        TestTable.query("select id,title from document_jp limit 5",null,function(error,result){
+        this.Table.Test.query("select id,title from document_jp limit 5",null,function(error,result){
 
             cont.ro.setData("data_t1",JSON.stringify(result,null,"　"));
 
             cont.next();
         });
-*/
+
+    }
+
+    _pattern2(){
 
         var cont=this;
 
         this.wait();
 
-        TestTable.select()
+        this.Table.Test.select()
             .field(["id","title","url"])
             .count(function(error,result){
 
@@ -44,6 +56,37 @@ module.exports = class TableController extends Controller{
             })
         ;
 
+    }
 
+    _show(){
+
+        
+        console.log(this.Table.Test.show());
+
+    }
+
+    _save1(){
+
+        var save={
+            aaa:"bbbbbbbbb",
+            ccc:"dddddddddddddd",
+        };
+
+        var cont=this;
+
+        this.Table.Test.save()
+            .insert(save,null,function(error,result){
+
+                if(error){
+                    cont.ro.debug(error);
+                }
+                else{
+                    cont.ro.debug(result);
+                }
+
+            })
+        ;
+
+        cont.ro.exit();
     }
 };
