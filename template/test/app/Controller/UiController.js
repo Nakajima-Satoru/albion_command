ee2e47215a4875be8903214b30595ec49378be85
nakajima-satoru <nakajima@email.com>
2021-05-03 10:20:20 +0900

    5/3

--- template/test/app/Controller/UiController.js
+++ template/test/app/Controller/UiController.js
@@ -4,6 +4,13 @@ module.exports = class UiController extends AppController{
 
 	form(){
 		this.Ui.load("Form");
+
+
+		if(this.ro.post.get()){
+			var post = this.ro.post.get();
+
+			this.ro.debug(post).exit();
+		}
 	}
 
 };
